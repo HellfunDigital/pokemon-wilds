2b98752c5ec98eb929383bd853726fda16ba9b0c
Hellfun Digital <47078754+HellfunDigital@users.noreply.github.com>
2025-09-21 23:39:18 +1000

    Ignore Python cache artifacts

---
+++ gym_builder/__init__.py
@@ -0,0 +1,11 @@
+"""Gym building toolkit for managing custom Pokemon gyms."""
+
+from .gym import Gym, GymBuilder, Trainer, Pokemon, Room
+
+__all__ = [
+    "Gym",
+    "GymBuilder",
+    "Trainer",
+    "Pokemon",
+    "Room",
+]
